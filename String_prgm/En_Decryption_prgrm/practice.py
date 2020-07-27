126f2fe1e1358b66522bd4802d08e95bfaa5a6ef
kay513 <66692952+kay513@users.noreply.github.com>
2020-07-27 22:44:57 +0530

    program.py
    this program uses Ceasar method to encrypt and decrypt words / sentences. Save the word.text file in the same folder as this file . This program picks up the best word that form meaning  out of the words given in "word.txt" file t0 decrypt a string

---
+++ String_prgm/En_Decryption_prgrm/practice.py
@@ -0,0 +1,10 @@
+Ulist = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ")
+num = list(range(1,27))
+Llist = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
+udict = { Ulist[i] : num[i] for i in range(0,26)}
+ldict = { Llist[i] : num[i] for i in range(0,26)}
+print(udict , ldict)
+
+
+        
+
